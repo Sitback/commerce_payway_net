0e9b3b37a3418cff9dd62b56fbe0bfad1561e8cb
Denis Kolmerschlag <denis@sitback.com.au>
2017-06-15 13:39:00 +1000

    Remove setter and getter of configuration, it’s inherited from parent

--- src/Plugin/Commerce/PaymentGateway/PayWayNetGateway.php
+++ src/Plugin/Commerce/PaymentGateway/PayWayNetGateway.php
@@ -28,28 +28,6 @@ use Symfony\Component\HttpFoundation\Request;
 class PayWayNetGateway extends OffsitePaymentGatewayBase {
 
     /**
-     * Gets this plugin's configuration.
-     *
-     * @return array
-     *   An array of this plugin's configuration.
-     */
-    public function getConfiguration()
-    {
-        // TODO: Implement getConfiguration() method.
-    }
-
-    /**
-     * Sets the configuration for this plugin instance.
-     *
-     * @param array $configuration
-     *   An associative array containing the plugin's configuration.
-     */
-    public function setConfiguration(array $configuration)
-    {
-        // TODO: Implement setConfiguration() method.
-    }
-
-    /**
      * Gets default configuration for this plugin.
      *
      * @return array
