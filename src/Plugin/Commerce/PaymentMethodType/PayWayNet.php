e75ae2eb1b7874e175d24cb78b159ce028f3630f
Denis Kolmerschlag <denis@sitback.com.au>
2017-06-15 11:26:19 +1000

    Create PayWayNet paymentMethodType

--- src/Plugin/Commerce/PaymentMethodType/PayWayNet.php
+++ src/Plugin/Commerce/PaymentMethodType/PayWayNet.php
@@ -2,6 +2,42 @@
 
 namespace Drupal\commerce_payway_net\Plugin\Commerce\PaymentMethodType;
 
-class PayWayNet {
+use Drupal\commerce_payment\Entity\PaymentMethodInterface;
+use Drupal\commerce_payment\Plugin\Commerce\PaymentMethodType\PaymentMethodTypeBase;
+use Drupal\commerce\BundleFieldDefinition;
 
+/**
+ * Provides the PayPal payment method type.
+ *
+ * @CommercePaymentMethodType(
+ *   id = "paywaynet",
+ *   label = @Translation("PayWay"),
+ *   create_label = @Translation("PayWay")
+ * )
+ */
+class PayWayNet extends PaymentMethodTypeBase{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildLabel(PaymentMethodInterface $payment_method) {
+        $args = [
+            '@paywaynet_token' => $payment_method->paywaynet_token->value,
+        ];
+        return $this->t('PayWayNet token (@paywaynet_token)', $args);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildFieldDefinitions() {
+        $fields = parent::buildFieldDefinitions();
+
+        $fields['paywaynet_token'] = BundleFieldDefinition::create('string')
+            ->setLabel(t('PayWayNet Token'))
+            ->setDescription(t('The PayWayNet token associated with the credit card.'))
+            ->setRequired(TRUE);
+
+        return $fields;
+    }
 }
