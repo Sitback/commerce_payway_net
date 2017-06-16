7f20458bb053a41db896e4b8feb3708a09aa07b4
Denis Kolmerschlag <denis@sitback.com.au>
2017-06-16 11:49:24 +1000

    Make the cUrl call to get the token

--- src/PluginForm/PayWayNetForm.php
+++ src/PluginForm/PayWayNetForm.php
@@ -34,6 +34,44 @@ class PayWayNetForm extends PaymentGatewayFormBase {
     public function buildConfigurationForm(array $form, FormStateInterface $form_state)
     {
         // TODO: Implement buildConfigurationForm() method.
+
+        /** @var \Drupal\commerce_payment\Entity\PaymentInterface $payment */
+        $payment = $this->entity;
+        /** @var \Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayInterface $payment_gateway_plugin */
+        $payment_gateway_plugin = $payment->getPaymentGateway()->getPlugin();
+        $configuration = $payment_gateway_plugin->getConfiguration();
+        $baseUrl = $configuration['commerce_payway_net_payWayBaseUrl'];
+
+        // 1. Generate token.
+        // www.payway.com.au/RequestToken
+        $param1 = "biller_code=" . $configuration['commerce_payway_net_billerCode'];
+        $param2 = "&username=" . $configuration['commerce_payway_net_username'];
+        $param3 = "&password=" . $configuration['commerce_payway_net_password'];
+        $params = $param1 . $param2 . $param3;
+
+        $ch = curl_init($baseUrl . "RequestToken");
+        curl_setopt_array($ch, array(
+            CURLOPT_POST => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_HTTPHEADER => array('Content-Type: application/x-www-form-urlencoded'),
+            CURLOPT_POSTFIELDS => $params,
+        ));
+
+        // Make the request.
+        // token=xxxxxx.
+        $token = curl_exec($ch);
+
+        // Check the response for errors.
+        $errorNumber = curl_errno($ch);
+        if ($errorNumber !== 0) {
+            $errorMessage = curl_error($ch);
+            \Drupal::logger('commerce_payway_net')->error($errorMessage);
+            header("HTTP/1.1 403 " . $errorMessage);
+            exit;
+        }
+        curl_close($ch);
+
+
         $a = 1;
     }
 
@@ -51,6 +89,6 @@ class PayWayNetForm extends PaymentGatewayFormBase {
     public function submitConfigurationForm(array &$form, FormStateInterface $form_state)
     {
         // TODO: Implement submitConfigurationForm() method.
-        $a = 1;
+        $a=1;
     }
 }
